7b8f37a3eb42b659f5bc7bc29b8d427625a13052
John Bogil <johncbogil@gmail.com>
2017-10-26 17:30:33 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+
+class SearchPage extends Component {
+  render() {
+    return <Text style={styles.description}>Search for houses to buy!</Text>;
+}
+}
+  class PropertyFinder extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
+
+export default PropertyFinder;
