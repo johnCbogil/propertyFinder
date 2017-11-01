a8de35bda8012008563c27de5173f43554be8e70
John Bogil <johncbogil@gmail.com>
2017-11-01 19:16:48 -0400

    Created propertydetail file

---
+++ PropertyDetail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableHighlight,
+  FlatList,
+  Text,
+} from 'react-native';
+
+class PropertyDetail extends Component {
+
+  _onPress = () => {
+    // this.props.onPressItem(this.props.index);
+  }
+
+
+}
